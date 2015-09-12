a251f064b381b7af3ca927bf1c13352cad0e636d
smjb <smjb@users.noreply.github.com>
2015-09-12 17:02:05 +0800

    Added Table View

--- ui.R
+++ ui.R
@@ -1,9 +1,34 @@
+###
+# File     : ui.R
+# Author   : smjb
+# Context  : shiny application
+#   AppName: Simple Analytics of Japanese Population, 1970 - 2010
+#   For    : Coursera Data Scientist Specialization
+#   For    : Module 9 - Developing Data Products
+#   Date   : Sept 2015
+# Remark   : rCharts not used as it has unpredictable errors that is time consuming to 
+#          : be debugged. The effort is too much for simple application like this
+###
+
 # The user-interface definition of the Shiny web app.
 library(shiny)
 library(rCharts)
 library(dplyr)
 
-source("reloadData.R")
+#source("reloadData.R")
+
+# line only used during debugging. 
+options(shiny.error=browser) 
+
+## UI Layout Brief
+# UI has a Top Navigation Bar, with 4 Navigation Panel : 
+# 1. Population View by Prefecture (Prefecture View)
+# 2. Age Tracking by Prefecture (Age Tracking)
+# 3. Age Group Statistics on Census Year (Age Group View)
+# 4. Age Group Evolution in Time (Age Group Evolution)
+# 4. Raw Data View (Table View)
+## --
+
 
 shinyUI(
   fluidPage(
@@ -11,7 +36,7 @@ shinyUI(
     navbarPage(
       "Japan Population Census",
       tabPanel(
-        "Age Group View", 
+        "Population View", 
         sidebarLayout(
           position = "right",
           sidebarPanel(
@@ -25,29 +50,14 @@ shinyUI(
             ), #sliderInput.timeframe
             selectInput(
               "pref", 
-              label = h3("prefecture"), 
+              label = h3("Select Prefecture"), 
               choices = listPref(),
               selected = 0
-            ), #selectInput.pref
-            selectInput(
-              "agegroup", 
-              label = h3("AgeGroup"), 
-              choices = listAgeGroup(),
-              selected = 1
-            ) #selectinput.agegroup
+            ) #selectInput.pref
           ), #sideBarPanel.control
           mainPanel(
-            #             textOutput("timerange"),
-            #             textOutput("prefecture_selected"),
-            #             #showOutput("chart100pct_pref", "nvd3"),
-            #             textOutput("age_selected"),
-            #             #showOutput("AgeEvolution", "polycharts"),
-            #plotOutput("showAgeGroupBox"),
             plotOutput("chart_pref"),
             plotOutput("chart100pct_pref")
-            #             
-            #             #showOutput("prefchart", "nvd3"),
-            #             textOutput("agegroup_selected")
           )#mainpanel
         )#SiteBarLayout.right
       ),#Tabpanel.tab1
@@ -66,7 +76,7 @@ shinyUI(
             ), #sliderInput.timeframe2
             selectInput(
               "pref2", 
-              label = h3("prefecture"), 
+              label = h3("Select Prefecture"), 
               choices = listPref(),
               selected = 0
             )
@@ -85,7 +95,7 @@ shinyUI(
         )#SiteBarLayout.right
       ),#Tabpanel.tab2
       tabPanel( 
-        "Age Group ", 
+        "Age Group View", 
         sidebarLayout(
           position = "right",
           sidebarPanel(
@@ -98,16 +108,12 @@ shinyUI(
             )
           ), #sideBarPanel.control
           mainPanel(
-            #            h1("Distribution of Prefectures Population by Age Group"),
-            #           p("xyz"),
-            #          h4("View by population"),
-            plotOutput("showAgeGroupBoxPct"),
-            h4("% ")
+            plotOutput("showAgeGroupBoxPct")
           )#mainpanel
         )#SiteBarLayout
       ),#Tabpanel.tab3
       tabPanel( 
-        "Age Group Evolution in Time", 
+        "Age Group Evolution", 
         sidebarLayout(
           position = "right",
           sidebarPanel(
@@ -120,15 +126,21 @@ shinyUI(
             )
           ), #sideBarPanel.control
           mainPanel(
-            #            h1("Distribution of Prefectures Population by Age Group"),
-            #           p("xyz"),
-            #          h4("View by population"),
-            #            plotOutput("showAgeGroupEvolutionBox"),
-            plotOutput("showAgeGroupEvolutionBoxPct"),
-            h4("% ")
+            plotOutput("showAgeGroupEvolutionBoxPct")
           )#mainpanel
         )#SiteBarLayout
-      )#Tabpanel.tab4
+      ),#Tabpanel.tab4
+      tabPanel( 
+        "Table View", 
+        mainPanel(
+          h1("Hello world"),
+          fluidRow(
+            column(10,
+                   dataTableOutput('dispStatTable')
+            ) # column
+          ) #fluid row
+        ) #main
+      )#Tabpanel.tab5      
     ) #navPanel
   )#fluidpage
 )#shiny
